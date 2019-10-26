44258fdb902100924af33dab19d34726fbb3b599
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-10-27 02:49:51 +0700

    Add Recipe

--- structured_data/recipe.py
+++ structured_data/recipe.py
@@ -1,10 +1,13 @@
 """
 https://developers.google.com/search/docs/data-types/recipe
 """
-from typing import Union, Optional
+from typing import Iterable, Optional, Union
 
-from structured_data.base import Thing, Number
-from structured_data.lower.quantity import Energy, EnergyUnit, Mass, MassUnit
+from structured_data.base import URL, Date, Number, Property, Text, Thing
+from structured_data.lower.quantity import Duration, Energy, EnergyUnit
+from structured_data.person import Person
+from structured_data.review import AggregateRating
+from structured_data.video import VideoObject
 
 
 class NutritionInformation(Thing):
@@ -63,6 +66,203 @@ class NutritionInformation(Thing):
         if isinstance(calories, Number):
             calories = Energy(calories, EnergyUnit.CALORIE)
         self._calories: Energy = calories
+
+
+class Recipe(Thing):
+    """
+    Required properties:
+        image: URL, ImageObject
+            Image of the completed dish.
+
+            Additional image guidelines:
+              - Every page must contain at least one image (whether or not you include markup). Google will pick the best image to display in Search results based on the aspect ratio and resolution.
+              - Image URLs must be crawlable and indexable.
+              - Images must represent the marked up content.
+              - Images must be in .jpg, .png, or. gif format.
+              - For best results, provide multiple high-resolution images (minimum of 50K pixels when multiplying width and height) with the following aspect ratios: 16x9, 4x3, and 1x1.
+            For example:
+            ```
+            "image": [
+              "https://example.com/photos/1x1/photo.jpg",
+              "https://example.com/photos/4x3/photo.jpg",
+              "https://example.com/photos/16x9/photo.jpg"
+            ]
+            ```
+        name: Text
+            The name of the dish.
+
+    Recommended properties:
+        aggregateRating: AggregateRating
+            Annotation for the average review score assigned to the item.
+            Follow the Review snippet guidelines and list of required
+            and recommended AggregateRating properties.
+            If the recipe structured data contains a single review, the
+            reviewer’s name must be a valid person or organization.
+            For example, "50% off ingredients" is not a valid name for
+            a reviewer.
+        author: Person
+            Creator of the recipe.
+        cookTime: Duration
+            The time it takes to actually cook the dish in ISO 8601
+            format. You can use min and max as child elements to
+            specify a range of time.
+            Always use in combination with prepTime.
+        datePublished: Date
+            The date the recipe was published in ISO 8601 format.
+        description: Text
+            A short summary describing the dish.
+        keywords: Text
+            Other terms for your recipe such as the season ("summer"),
+            the holiday ("Halloween"), or other descriptors
+            ("quick", "easy", "authentic").
+            Additional guidelines:
+              - Separate multiple entries in a keywords list with commas.
+              - Don't use a tag that should be in ``recipeCategory``
+                or ``recipeCuisine``.
+            Not recommended:
+                "keywords": "dessert, American"
+            Recommended:
+                "keywords": "winter apple pie, nutmeg crust"
+        nutrition.calories: Energy
+            The number of calories in each serving.
+        prepTime: Duration
+            The length of time it takes to prepare the dish, in ISO
+            8601 format. You can use min and max as child elements to
+            specify a range of time.
+            Always use in combination with cookTime.
+        recipeCategory: Text
+            The type of meal or course your recipe is about.
+            For example: "dinner", "entree", or "dessert, snack".
+        recipeCuisine: Text
+            The region associated with your recipe. For example,
+            "French", Mediterranean", or "American".
+        recipeIngredient: Text
+            An ingredient used in the recipe. This property is
+            recommended for recipes on Google Search, but it's required
+            for guidance with the Google Assistant on Google Home and
+            mart displays.
+            Additional guidelines:
+              - Include only the ingredient text that is necessary for
+                making the recipe.
+              - Don't include unnecessary information, such as a
+                definition of the ingredient.
+        recipeInstructions: Text
+            The steps to make the dish. This property is recommended
+            for recipes on Google Search, but it's required for
+            guidance with the Google Assistant on Google Home and smart
+            displays.
+            There are several options for setting the value of
+            ``recipeInstructions``.
+            We recommend using ``HowToStep`` or ``HowToSection``:
+              - HowToStep: You can specify the exact text for each step
+                sentence by setting the value of each HowToStep. This
+                provides hints to the Google Assistant, but note it may
+                group the recipe steps differently based on the context.
+              - HowToSection (only if a recipe has multiple sections):
+                Use to group steps into multiple sections. Provide the
+                section name in its name property (for example, "Make the
+                crust") and specify each HowToStep as an itemListElement.
+                Set the text property of each HowToStep.
+                For example, a pizza recipe may have one section of steps
+                for making the crust, one for preparing the toppings, and
+                one for combining and baking. If you don't indicate
+                multiple sections, the Google Assistant may mistakenly
+                present the section name as just another step (for example,
+                a "Make the crust" step followed by a "Combine the
+                flour and yeast" step).
+              - Single or repeated property of text: A block of text
+                that includes one or more steps. Google treats all steps
+                as being in a single section. Repeated property values
+                are concatenated into a single block of text. Google
+                then attempts to automatically split the single block
+                of text into individual steps. Google tries to find and
+                remove any section names, step numbers, keywords, and
+                anything else that can incorrectly appear in recipe step
+                text. For best results, we recommend you unambiguously
+                specify individual step sentences with HowToStep, as
+                described above.
+            Additional guidelines
+              - Don't include metadata that belongs elsewhere.
+                In particular, use the author property to specify the
+                author, recipeCuisine for cuisine, recipeCategory for
+                category, and keywords for other keywords.
+              - Include only text on how to make the recipe and don't
+                include other text such as "Directions", "Watch the video",
+                "Step 1". Those phrases should be specified outside of
+                the structured data.
+        recipeYield: Text
+            The quantity produced by the recipe.
+            For example: number of people served, or number of servings.
+        totalTime: Duration
+            The total time it takes to prepare the cook the dish, in
+            ISO 8601 format. You can use min and max as child elements
+            to specify a range of time.
+            Use totalTime or a combination of both cookTime and prepTime.
+        video: VideoObject
+            An array of video properties that depict the recipe on the page.
+            Follow the list of required and recommended Video properties.
+    """
+    PROPERTIES = (
+        # required
+        'name',
+        'image',
+        # recommended
+        'author',
+        'datePublished',
+        'description',
+        'prepTime',
+        'cookTime',
+        'totalTime',
+        'keywords',
+        'recipeYield',
+        'recipeCategory',
+        'recipeCuisine',
+        'nutrition',
+        'recipeIngredient',
+        'recipeInstructions',
+        'aggregateRating',
+        'video',
+    )
+
+    def __init__(
+            self,
+            *,
+            image: Iterable[Union[URL, str]],
+            name: Text,
+            aggregateRating: Optional[AggregateRating] = None,
+            author: Optional[Person] = None,
+            cookTime: Optional[Duration] = None,
+            datePublished: Optional[Date] = None,
+            description: Optional[Text] = None,
+            keywords: Optional[Text] = None,
+            nutrition: Optional[NutritionInformation] = None,
+            prepTime: Optional[Duration] = None,
+            recipeCategory: Optional[Text] = None,
+            recipeCuisine: Optional[Text] = None,
+            recipeIngredient: Optional[Iterable[Text]] = None,
+            recipeInstructions: Optional[Iterable[Text]] = None,  # TODO
+            recipeYield: Optional[Text] = None,
+            totalTime: Optional[Duration] = None,
+            video: Optional[VideoObject] = None,
+            **kwargs):
+        self._image: Property[URL] = Property(*image, class_=URL)
+        self._name: Text = name
+        self.name: Text = name
+        self._aggregate_rating = aggregateRating
+        self._author = author
+        self._cook_time = cookTime
+        self._date_published = datePublished
+        self._description = description
+        self._keywords = keywords
+        self._nutrition = nutrition
+        self._prep_time = prepTime
+        self._recipe_category = recipeCategory
+        self._recipe_cuisine = recipeCuisine
+        self._recipe_ingredient = recipeIngredient
+        self._recipe_instructions = recipeInstructions
+        self._recipe_yield = recipeYield
+        self._total_time = totalTime
+        self._video = video
 
 
 if __name__ == '__main__':
