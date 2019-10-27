1d34e96c80c37e3452d38849e5d177f26251533d
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-10-27 11:28:21 +0700

    Add JSON object: Logo boilerplate

---
+++ structured_data/logo.py
@@ -0,0 +1,22 @@
+"""
+https://developers.google.com/search/docs/data-types/logo
+"""
+from structured_data.base import Thing
+
+
+class Organization(Thing):
+    """
+    Required properties:
+
+        logo: URL
+        URL of a logo that is representative of the organization.
+
+        Additional image guidelines:
+          - The image must be 112x112px, at minimum.
+          - The image URL must be crawlable and indexable.
+          - The image must be in .jpg, .png, or. gif format.
+
+        url: URL
+
+        The URL of the website associated with the logo.
+    """
