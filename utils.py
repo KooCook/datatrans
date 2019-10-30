2e330202a56e40d9c8ace9a11387cbbd9af770d4
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-10-30 20:32:07 +0700

    Add more doctest to utils.snake_to_camel()

--- utils.py
+++ utils.py
@@ -12,6 +12,13 @@ def snake_to_camel(string: str) -> str:
     'snakeToCamel'
     >>> snake_to_camel('__snake_to_camel__')
     'snakeToCamel'
+    >>> snake_to_camel('snakeToCamel')
+    'snakeToCamel'
     """
     first, *others = [word for word in string.split('_') if word]
     return first + ''.join(s.title() for s in others)
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
