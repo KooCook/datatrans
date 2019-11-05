0a6344534f876c6a4a5b20547794b7cd5c5a1ad7
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-05 23:08:29 +0700

    Remove redundant try statement

--- datatrans/utils/functions.py
+++ datatrans/utils/functions.py
@@ -84,41 +84,38 @@ def parse_vulgar_fractions(string: str) -> float:
     """
     if not isinstance(string, str):
         raise TypeError('invalid argument of type \'{}\''.format(string.__class__.__name__))
-    try:
-        if '⅟' in string:
-            *number, fraction = string.split('⅟')
-            fraction = 1 / float(fraction)
-        else:
-            *number, fraction = list(string)
-            try:
-                fraction = {
-                    '¼': 0.25,
-                    '½': 0.5,
-                    '¾': 0.75,
-                    '⅐': 1/7,
-                    '⅑': 1/9,
-                    '⅓': 1/3,
-                    '⅔': 2/3,
-                    '⅕': 0.2,
-                    '⅖': 0.4,
-                    '⅗': 0.6,
-                    '⅘': 0.8,
-                    '⅙': 1/6,
-                    '⅛': 0.125,
-                    '⅜': 0.375,
-                    '⅝': 0.625,
-                    '⅞': 0.875,
-                    '⅟': None,
-                    '↉': 0,
-                }[fraction]
-            except KeyError as e:
-                raise ValueError('invalid string \'{}\', '
-                                 '\'{}\' is not a vulgar fraction'
-                                 .format(string, fraction)) from e.__context__
-        number = float(''.join(number)) if number else 0
-        return number + fraction
-    except ValueError as e:
-        raise
+    if '⅟' in string:
+        *number, fraction = string.split('⅟')
+        fraction = 1 / float(fraction)
+    else:
+        *number, fraction = list(string)
+        try:
+            fraction = {
+                '¼': 0.25,
+                '½': 0.5,
+                '¾': 0.75,
+                '⅐': 1/7,
+                '⅑': 1/9,
+                '⅓': 1/3,
+                '⅔': 2/3,
+                '⅕': 0.2,
+                '⅖': 0.4,
+                '⅗': 0.6,
+                '⅘': 0.8,
+                '⅙': 1/6,
+                '⅛': 0.125,
+                '⅜': 0.375,
+                '⅝': 0.625,
+                '⅞': 0.875,
+                '⅟': None,
+                '↉': 0,
+            }[fraction]
+        except KeyError as e:
+            raise ValueError('invalid string \'{}\', '
+                             '\'{}\' is not a vulgar fraction'
+                             .format(string, fraction)) from e.__context__
+    number = float(''.join(number)) if number else 0
+    return number + fraction
 
 
 if __name__ == '__main__':
