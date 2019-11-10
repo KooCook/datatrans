73fe331527c6ecb4a7ea068c4b19429e697c8f5b
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-10 19:08:41 +0700

    Add `camel_to_snake`
    Also changed all parameters with name `string` to `s` to avoid conflict with standard library `string`

--- datatrans/utils/functions.py
+++ datatrans/utils/functions.py
@@ -1,5 +1,6 @@
 import re
-from typing import Iterable, List, Match, Optional
+import string
+from typing import Iterable, Match, Optional
 
 NUMERAL = {
     'one': 1,
@@ -17,8 +18,8 @@ NUMERAL = {
 }
 
 
-def snake_to_camel(string: str) -> str:
-    """Returns a new str in camelCase, given an str in snake case.
+def snake_to_camel(s: str) -> str:
+    """Returns a new str in camelCase, given an str in snake_case.
     Removes all underscore before and after.
 
     Examples:
@@ -28,12 +29,38 @@ def snake_to_camel(string: str) -> str:
         'snakeToCamel'
         >>> snake_to_camel('snakeToCamel')
         'snakeToCamel'
+        >>> snake_to_camel('lonesnake')
+        'lonesnake'
     """
-    first, *others = [word for word in string.split('_') if word]
+    first, *others = [word for word in s.split('_') if word]
     return first + ''.join(s.title() for s in others)
 
 
-def trim_spaces(string: str) -> str:
+def camel_to_snake(s: str) -> str:
+    """Returns a new str in snake_case, given an str in camelCase.
+
+    Raises:
+        ValueError: When original str is not in camelCase
+
+    Examples:
+        >>> camel_to_snake('camelToSnake')
+        'camel_to_snake'
+        >>> camel_to_snake('camel_to_snake')
+        Traceback (most recent call last):
+          ...
+        ValueError: original str is not in camelCase: 'camel_to_snake'
+        >>> camel_to_snake('lonecamel')
+        'lonecamel'
+    """
+    if '_' in s:
+        raise ValueError('original str is not in camelCase: \'{}\''
+                         .format(s))
+    for u in string.ascii_uppercase:
+        s = s.replace(u, '_' + u.lower())
+    return s
+
+
+def trim_spaces(s: str) -> str:
     """Trims excess spaces
 
     Examples:
@@ -44,10 +71,10 @@ def trim_spaces(string: str) -> str:
         >>> trim_spaces(' salt  ')
         'salt'
     """
-    return ' '.join(_ for _ in string.split(' ') if _)
+    return ' '.join(_ for _ in s.split(' ') if _)
 
 
-def parse_str_unicode(string: str) -> str:
+def parse_str_unicode(s: str) -> str:
     """Returns `string` with literal unicode replaced with actual unicode.
 
     Examples:
@@ -56,19 +83,20 @@ def parse_str_unicode(string: str) -> str:
         >>> parse_str_unicode('¾ cup (1½ sticks) cold unsalted butter, cut into ¼-inch pieces')
         '¾ cup (1½ sticks) cold unsalted butter, cut into ¼-inch pieces'
     """
+
     def unicode_repl(matchobj: Match):
         m = matchobj.group(0)
         return eval("'" + m[2:] + "'")
 
     pattern = re.compile(r'\\u\d\d\d\d')
-    return pattern.sub(unicode_repl, string)
+    return pattern.sub(unicode_repl, s)
 
 
-def read_numeral(string: str) -> int:
+def read_numeral(s: str) -> int:
     """Converts string containing numeral under 13 to `int`.
 
     Args:
-        string (str): positional only. string to parse
+        s (str): positional only. string to parse
 
     Examples:
         >>> read_numeral('One')
@@ -97,32 +125,32 @@ def read_numeral(string: str) -> int:
         12
     """
     try:
-        return NUMERAL[''.join(string.lower().split(' '))]
+        return NUMERAL[''.join(s.lower().split(' '))]
     except KeyError as e:
         raise ValueError('cannot parse \'{}\' as numerals'
-                         .format(string)) from e.__context__
+                         .format(s)) from e.__context__
 
 
-def _parse_unicode_vulgar(string: str) -> float:
-    if '⅟' in string:
-        *number, fraction = string.split('⅟')
+def _parse_unicode_vulgar(s: str) -> float:
+    if '⅟' in s:
+        *number, fraction = s.split('⅟')
         fraction = 1 / float(fraction)
     else:
-        *number, fraction = list(string)
+        *number, fraction = list(s)
         try:
             fraction = {
                 '¼': 0.25,
                 '½': 0.5,
                 '¾': 0.75,
-                '⅐': 1/7,
-                '⅑': 1/9,
-                '⅓': 1/3,
-                '⅔': 2/3,
+                '⅐': 1 / 7,
+                '⅑': 1 / 9,
+                '⅓': 1 / 3,
+                '⅔': 2 / 3,
                 '⅕': 0.2,
                 '⅖': 0.4,
                 '⅗': 0.6,
                 '⅘': 0.8,
-                '⅙': 1/6,
+                '⅙': 1 / 6,
                 '⅛': 0.125,
                 '⅜': 0.375,
                 '⅝': 0.625,
@@ -133,12 +161,12 @@ def _parse_unicode_vulgar(string: str) -> float:
         except KeyError as e:
             raise ValueError('invalid string \'{}\', '
                              '\'{}\' is not a vulgar fraction'
-                             .format(string, fraction)) from e.__context__
+                             .format(s, fraction)) from e.__context__
     number = float(''.join(number)) if number else 0
     return number + fraction
 
 
-def parse_vulgar_fractions(string: str) -> float:
+def parse_vulgar_fractions(s: str) -> float:
     """Converts ``string`` containing vulgar fractions to ``float``.
 
     Raises:
@@ -169,18 +197,18 @@ def parse_vulgar_fractions(string: str) -> float:
         >>> parse_vulgar_fractions('1/2')
         0.5
     """
-    if not isinstance(string, str):
-        raise TypeError('invalid argument of type \'{}\''.format(string.__class__.__name__))
-    if '/' in string:
+    if not isinstance(s, str):
+        raise TypeError('invalid argument of type \'{}\''.format(s.__class__.__name__))
+    if '/' in s:
         try:
-            numerator, denominator = string.split('/')
+            numerator, denominator = s.split('/')
             return float(numerator) / float(denominator)
         except ValueError:
             pass
-    return _parse_unicode_vulgar(string)
+    return _parse_unicode_vulgar(s)
 
 
-def get_closest_match(string: str, subs: Iterable[str]) -> Optional[str]:
+def get_closest_match(s: str, subs: Iterable[str]) -> Optional[str]:
     """
 
     Examples:
@@ -202,12 +230,12 @@ def get_closest_match(string: str, subs: Iterable[str]) -> Optional[str]:
     import difflib
     matches = []
     for sub in subs:
-        if sub in string:
+        if sub in s:
             matches.append(sub)
     if matches:
-        return difflib.get_close_matches(string, matches, cutoff=0)[0]
+        return difflib.get_close_matches(s, matches, cutoff=0)[0]
     try:
-        return difflib.get_close_matches(string, subs)[0]
+        return difflib.get_close_matches(s, subs)[0]
     except IndexError:
         return
 
@@ -225,4 +253,5 @@ def json_encoder(o):
 
 if __name__ == '__main__':
     import doctest
+
     doctest.testmod(optionflags=doctest.ELLIPSIS)
