8c4af8300422d37d2945b7c44782aefdd06dc42d
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-10 12:34:43 +0700

    Update FoodNutrient in nutrient.py

--- datatrans/fooddata/models/detail/nutrient.py
+++ datatrans/fooddata/models/detail/nutrient.py
@@ -40,15 +40,42 @@ class FoodNutrientDerivation(IdMixin, utils.DataClass):
     }
 
 
-    
-    __slots__ = ('type', 'id', 'nutrient', 'food_nutrient_derivation', 'amount')
 class FoodNutrient(IdMixin, utils.DataClass):
+    """A nutrient value for a food
+
+    Attributes:
+        id
+        fdc_id: ID of the food this food nutrient pertains to
+        nutrient: Nutrient to which the food nutrient pertains
+        amount: Amount of the nutrient per 100g of food.
+            Specified in unit defined in the nutrient table.
+        data_points: Number of observations on which the value is based
+        derivation_id: ID of the food nutrient derivation technique
+            used to derive the value
+        standard_error: Standard error
+        min: The minimum amount
+        max: The maximum amount
+        median: The median amount
+        footnote: Comments on any unusual aspects of the food nutrient.
+            Examples might include why a nutrient value is different
+            than typically expected.
+        min_year_acquired: Minimum purchase year of all acquisitions
+            used to derive the nutrient value
+        nutrient_analysis_details: Details of the analysis of the
+            nutrient (API only)
+    """
+
+    __slots__ = ('type', 'id', 'nutrient', 'data_points', 'min', 'food_nutrient_derivation', 'max', 'min', 'amount')
 
     __attr__ = (
         ('type', str),  # FoodNutrient
         ('id', int),
         ('nutrient', Nutrient),
+        ('data_points', int),
+        ('min', float),
         ('food_nutrient_derivation', FoodNutrientDerivation),
+        ('max', float),
+        ('min', float),
         ('amount', float),
     )
 
