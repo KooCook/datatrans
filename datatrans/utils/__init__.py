fe3cf496e0df0146b623bf2e2ef7c19e3cad3baf
Mai Mee <47658413+MaiMee1@users.noreply.github.com>
2019-11-11 14:11:28 +0700

    Fix circular import issue

--- datatrans/utils/__init__.py
+++ datatrans/utils/__init__.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
+BASE_DIR = Path(__file__).parent.parent.parent
+
 from .classes import *
 from .functions import *
 import datatrans.utils.fooddata as fooddata
 import datatrans.utils.structured_data as schema
-
-
-BASE_DIR = Path(__file__).parent.parent.parent
